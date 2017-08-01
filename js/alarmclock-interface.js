ad5ce40348c63248298906233a8c3e33b8b7add2
Alena Golovina and Emilie Thoreson <emilie@>
2017-08-01 16:12:02 -0700

    progress with alert logic

--- js/alarmclock-interface.js
+++ js/alarmclock-interface.js
@@ -1,12 +1,26 @@
-//var AlarmClock = require('./../js/alarmclock.js').alarmClockModule;
+var AlarmClock = require('./../js/alarmclock.js').alarmClockModule;
 
+AlarmClock.prototype.displayAlarm = function(month, day, year, hour, minute, second) {
+  $('.showAlarm').text("Your alarm is set for " + month + " " + day + ", " + year + " " + hour + ":" + minute + ":" + second);
+}
+
+$('#time').text();
+function update() {
+  $("#time").html(moment().format('M D YYYY H:mm:ss'));
+}
+setInterval(update, 1000);
 
 $(document).ready(function(){
-  $('#time').text();
-
-  function update() {
-    $("#time").html(moment().format('MMMM D YYYY H:mm:ss'));
-  }
-
-  setInterval(update, 1000);
+  var testClock = new AlarmClock(month, day, year, hour, minute, second);
+  $('#set-alarm').click(function() {
+    var month = parseInt($('#month').val());
+    var day = parseInt($('#day').val());
+    var year = parseInt($('#year').val());
+    var hour = parseInt($('#hour').val());
+    var minute = parseInt($('#minute').val());
+    var second = parseInt($('#second').val());
+    testClock.displayAlarm(month, day, year, hour, minute, second);
+    testClock.alarmClock(month, day, year, hour, minute, second);
+  setInterval(AlarmClock, 1000);
+  });
 });
--- js/alarmclock.js
+++ js/alarmclock.js
@@ -1,9 +1,36 @@
-function AlarmClock(currentTime) {
-  this.currentTime = currentTime;
+function AlarmClock() {
+  AlarmClock.prototype.alarmClock = function(month, day, year, hour, minute, second) {
+    var currentMonth = parseInt(moment().format("M"));
+    var currentDay = parseInt(moment().format("D"));
+    var currentYear = parseInt(moment().format("YYYY"));
+    var currentHour = parseInt(moment().format("H"));
+    var currentMinute = parseInt(moment().format("mm"));
+    var currentSecond = parseInt(moment().format("ss"));
+    console.log("Month:");
+    console.log(currentMonth);
+    console.log(month);
+    console.log("Day:");
+    console.log(currentDay);
+    console.log(day);
+    console.log("Year:");
+    console.log(currentYear);
+    console.log(year);
+    console.log("HOUR:");
+    console.log(currentHour);
+    console.log(hour);
+    console.log("MINUTE:");
+    console.log(currentMinute);
+    console.log(minute);
+    console.log("SECOND:");
+    console.log(currentSecond);
+    console.log(second);
+    if (currentMonth === month) {
+      console.log("passes this point!");
+    }
+    if ((currentMonth === month) && (currentDay === day) && (currentYear === year) && (currentHour === hour) && (currentMinute === minute) && (currentSecond === second)) {
+      console.log("WAKE UP!!! BUZZ BUZZ BUZZ");
+    }
+    setInterval(AlarmClock, 1000);
+  }
 }
-
-AlarmClock.prototype.alarmClock = function() {
-
-}
-
 exports.alarmClockModule = AlarmClock;
